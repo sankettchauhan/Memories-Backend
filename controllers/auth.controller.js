ee85ea2c321bccfbca9025a29c54c49e53ae0b3a
sankettchauhan <sanket.chauhan4@gmail.com>
2022-05-11 02:27:57 +0530

    update to not send otp

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -34,6 +34,20 @@ exports.createNewUser = async (req, res, next) => {
     // save user
     let user = await createUser.save();
 
+    // // generate otp
+    // const otp = generateOTP(6);
+    // // save otp to user collection
+    // user.phoneOtp = otp;
+    // await user.save();
+    // // send otp to phone number
+    // await fast2sms(
+    //   {
+    //     message: `Your OTP is ${otp}`,
+    //     contactNumber: user.phone,
+    //   },
+    //   next
+    // );
+
     res.status(201).json({
       type: "success",
       message: "Account created. OTP sent to mobile number",
@@ -41,20 +55,6 @@ exports.createNewUser = async (req, res, next) => {
         userId: user._id,
       },
     });
-
-    // generate otp
-    const otp = generateOTP(6);
-    // save otp to user collection
-    user.phoneOtp = otp;
-    await user.save();
-    // send otp to phone number
-    await fast2sms(
-      {
-        message: `Your OTP is ${otp}`,
-        contactNumber: user.phone,
-      },
-      next
-    );
   } catch (error) {
     next(error);
   }
@@ -72,20 +72,20 @@ exports.loginWithPhoneOtp = async (req, res, next) => {
       return;
     }
 
-    // generate otp
-    const otp = generateOTP(6);
-    // save otp to user collection
-    user.phoneOtp = otp;
-    user.isAccountVerified = true;
-    await user.save();
-    // send otp to phone number
-    await fast2sms(
-      {
-        message: `Your OTP is ${otp}`,
-        contactNumber: user.phone,
-      },
-      next
-    );
+    // // generate otp
+    // const otp = generateOTP(6);
+    // // save otp to user collection
+    // user.phoneOtp = otp;
+    // user.isAccountVerified = true;
+    // await user.save();
+    // // send otp to phone number
+    // await fast2sms(
+    //   {
+    //     message: `Your OTP is ${otp}`,
+    //     contactNumber: user.phone,
+    //   },
+    //   next
+    // );
 
     res.status(201).json({
       type: "success",
@@ -105,15 +105,15 @@ exports.verifyOTP = async (req, res, next) => {
   try {
     const { otp, userId } = req.body;
     const user = await User.findById(userId);
-    if (!user) {
-      next({ status: 400, message: USER_NOT_FOUND_ERR });
-      return;
-    }
+    // if (!user) {
+    //   next({ status: 400, message: USER_NOT_FOUND_ERR });
+    //   return;
+    // }
 
-    if (user.phoneOtp !== otp) {
-      next({ status: 400, message: INCORRECT_OTP_ERR });
-      return;
-    }
+    // if (user.phoneOtp !== otp) {
+    //   next({ status: 400, message: INCORRECT_OTP_ERR });
+    //   return;
+    // }
     const token = createJwtToken({ userId: user._id });
 
     // remove otp from database
